d00c41411910e87b5563636091d88861e351c003
David-396 <david.eini12@gmail.com>
2025-09-08 12:16:56 +0300

    finish logger

--- app/files_meta_pub/producer_config.py
+++ app/files_meta_pub/producer_config.py
@@ -1,3 +1,5 @@
+import os
+
 from kafka import KafkaProducer
 import json
 import logging
--- app/logger.py
+++ app/logger.py
@@ -1,13 +1,21 @@
 import logging
+import os
+from dotenv import load_dotenv
 from elasticsearch import Elasticsearch
 from datetime import datetime
 
+load_dotenv()
+
+LOGGER_NAME = os.getenv('LOGGER_NAME')
+ES_HOST = os.getenv('ES_HOST')
+LOGGER_INDEX_NAME = os.getenv('LOGGER_INDEX_NAME')
+ES_PORT = os.getenv('ES_PORT')
 
 class Logger:
     _logger = None
 
     @classmethod
-    def get_logger(cls, name:str, es_host:str,index:str, level=logging.DEBUG):
+    def get_logger(cls, name=LOGGER_NAME, es_host=ES_HOST, es_port=ES_PORT, index=LOGGER_INDEX_NAME, level=logging.DEBUG):
         if cls._logger:
             return cls._logger
 
@@ -15,7 +23,8 @@ class Logger:
         logger.setLevel(level)
 
         if not logger.handlers:
-            es = Elasticsearch(es_host)
+            es_uri = f'http://{es_host}:{es_port}'
+            es = Elasticsearch(es_uri)
 
             class ESHandler(logging.Handler):
                 def emit(self, record):
@@ -24,8 +33,7 @@ class Logger:
                                                         "level": record.levelname,
                                                         "logger": record.name,
                                                         "message": record.getMessage()
-
-                                })
+                                                        })
 
                     except Exception as e:
                         print(f"ES log failed: {e}")
